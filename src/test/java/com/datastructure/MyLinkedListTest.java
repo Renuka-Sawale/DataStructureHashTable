e1331f2373b5abbdeeeae1863804203bde7ddd99
Renuka Sawale <rrsuryawanshi11@gmail.com>
2021-05-19 00:15:56 +0530

    Added welcome message

---
+++ src/test/java/com/datastructure/MyLinkedListTest.java
@@ -0,0 +1,54 @@
+package com.datastructure;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyLinkedListTest {
+    @Test
+    public void given3NumbersWhenAddedToLinkedListShouldBeAddTop() {
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
+        myLinkedList.printMyNodes();
+        boolean result = myLinkedList.head.equals(myThirdNode) &&
+                         myLinkedList.head.getNext().equals(mySecondNode) &&
+                         myLinkedList.tail.equals(myFirstNode);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void given3NumbersWhenAppendedShouldBeAddedToLast() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.printMyNodes();
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                         myLinkedList.head.getNext().equals(mySecondNode) &&
+                         myLinkedList.tail.equals(myThirdNode);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void given3NumbersSearchKeyShouldPassLinkedListResult() {
+        MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+        MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+        MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        INode result = myLinkedList.search(mySecondNode);
+        System.out.println(result.getKey());
+        Assertions.assertEquals(mySecondNode.getKey(),result.getKey());
+    }
+
+
+}
